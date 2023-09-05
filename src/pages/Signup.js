6ef8574c8b21fb0d97a6374b0cbce03c64497df8
Temitope Balogun <tsbalogun97@yahoo.com>
2023-09-05 14:18:42 -0500

    updated the Signup.js file

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,25 @@
+import { useState } from 'react'
+
+
 const Signup = () => {
-  //using useState to keep track of what is being typed inside the signup field for the email & password
-  
+  //using useState to keep track of what is being typed inside the signup fields for the email & password
+  const [email, setEmail] = useState('')
+  const [Password, setPassword] = useState('')
+
+  return (
+    <form className="signup" onSubmit={}>
+      <h3>Sign up</h3>
+
+      <label>Email:</label>
+      <input 
+      
+      type="email" 
+      onChange={(e) => setEmail(e.target.value)}
+      //when the value changes whether typing into the field or delete a letter, you want it to update the email state to match it 
+      />
+
+    </form>
+
+  )
+
 }
