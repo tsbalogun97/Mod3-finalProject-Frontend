84a68539f024900fd9ff5ed23962b100c40c9b75
Temitope Balogun <tsbalogun97@yahoo.com>
2023-09-05 13:12:50 -0500

    added useAuthContext function

--- src/hooks/useAuthContext.js
+++ src/hooks/useAuthContext.js
@@ -1,12 +1,12 @@
-import { CarsContext } from "../context/CarContext";
+import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";// this will be used to consume the context
 
-export const useCarsContext = () => {
-  const context = useContext(CarsContext)//this is passed from the value from CarsContext.Provider from the CarsContext.js
+export const useAuthContext = () => {
+  const context = useContext(AuthContext)//this is passed from the value from AuthContextProvider from the useAuthContext.js
 
   if(!context) {
-    throw Error('useCarsContext must be used inside a CarsContextProvider')
+    throw Error('useAuthContext must be used inside an AuthContextProvider')
   }
 
-  return context // this means that anytime I want to use the data , I just will have to  evoke the useCarsContext hook to get that context value back.
+  return context // this means that anytime I want to use the data , I just will have to  evoke the useAuthContext hook to get that context value back.
 }
