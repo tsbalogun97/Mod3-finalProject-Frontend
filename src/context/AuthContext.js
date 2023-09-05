af9bc188289fa2070cf1628070f15f5e88cb5de7
Temitope Balogun <tsbalogun97@yahoo.com>
2023-09-05 12:56:44 -0500

    updated the AuthContext.js with the AuthContextProvider embedding the entire root app

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,7 +7,12 @@ export const authReducer = (state, action) => {
   //inside here, you want to handle different cases such as login and logout cases
   switch (action.type) {
     case 'LOGIN':
-      return { user: }// the user will be whatever payload is on the outside
+      return { user: action.payload }// the user will be whatever payload is on the action. this will be user you get back from the server 
+
+    case 'LOGOUT':
+      return { user: null }// when you logout, it goes back to null
+    default: 
+      return state//you are return the origin state if there' no changes
   }
 }
 
@@ -17,5 +22,14 @@ export const  AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null 
   })
+
+  console.log('AuthContext state: ', state)//this is how you keep track of things as a developer
+
+  return (
+    //wraps up the root app (children)
+    <AuthContext.Provider>
+      { children }
+    </AuthContext.Provider>
+  )
   
 }
