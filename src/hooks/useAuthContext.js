aafb11fc17c303c19e94185152bd3248bc362229
Temitope Balogun <tsbalogun97@yahoo.com>
2023-09-05 13:41:21 -0500

    created a Signup file inside pages folder

--- src/hooks/useAuthContext.js
+++ src/hooks/useAuthContext.js
@@ -1,12 +1,12 @@
 import { AuthContext } from "../context/AuthContext";
-import { useContext } from "react";// this will be used to consume the context
+import { useContext } from "react";
 
 export const useAuthContext = () => {
-  const context = useContext(AuthContext)//this is passed from the value from AuthContextProvider from the useAuthContext.js
+  const context = useContext(AuthContext)
 
   if(!context) {
     throw Error('useAuthContext must be used inside an AuthContextProvider')
   }
 
-  return context // this means that anytime I want to use the data , I just will have to  evoke the useAuthContext hook to get that context value back.
+  return context 
 }
---
+++ src/pages/Signup.js
@@ -0,0 +1,4 @@
+const Signup = () => {
+  //using useState to keep track of what is being typed inside the signup field for the email & password
+  
+}
