cbda0a8ab223ef722f586ade931ef38b353e5885
Temitope Balogun <tsbalogun97@yahoo.com>
2023-09-05 23:30:56 -0500

    updated login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,18 @@
 import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 
 const Login = () => {
   //using useState to keep track of what is being typed inside the login fields for the email & password
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [login, error, isLoading] = useLogin()
 
   const handleSubmit = async (e) => {
     //inside here you are taking the event object from the submit event. *when submitting a form, the default behavior is to refresh the page
     e.preventDefault()
-    console.log(email, password)
+    // console.log(email, password)
+    await login(email, password)
   }
 
   return (
@@ -30,7 +33,8 @@ const Login = () => {
         value={password}
       />
       
-      <button>Login</button>
+      <button disabled={isLoading}>Login</button>
+      {error && <div className="error">{error}</div>}
     </form>
     )
   
