1457ec0e6796bcb36913fc4cc840212a66b7e457
Temitope Balogun <tsbalogun97@yahoo.com>
2023-09-05 22:01:01 -0500

    update useSignup hook with more a function to save user in localstorage

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,2 +1,30 @@
 import { useState } from 'react'
 import { useAuthContext } from './useAuthContext'
+
+export const useSignup = () => {
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(null)
+
+  const signup = async (email, password) => {
+    setIsLoading(true)
+    setError(null)
+
+    const response = await fetch('/api/user/signup', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},// way to send data to the request body property.
+      body: JSON.stringify({email, password})
+    })
+    const json = await response.json()//when you get a response , in order to get a json data, you have to use a response.json method. This either going to return some information with the json web token if it was a success. but if it wasnt, then it will send back an error message
+
+    if (!response.ok) {
+      setIsLoading(false)
+      setError(json.error)
+    }
+    if (response.ok) {
+      // save the user to local storage that way their jwt is still stored somewhere in the browser even if they log in back and forth.
+      localStorage.setItem('user', )
+
+    }
+
+  }
+}
